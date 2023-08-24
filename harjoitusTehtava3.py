923012cb9d3b7b87208d7c187e462530fc153d23
its me <76001700+whoiszacky@users.noreply.github.com>
2023-08-24 11:02:01 +0300

    Create harjoitusTehtava3.py

---
+++ harjoitusTehtava3.py
@@ -0,0 +1,7 @@
+binary =input("anna 4 bitten binääriluku: ")
+desimaali = int(binary, 2)
+if len(binary) != 4:
+    print("sinun täytyy laittaa 4 binaarilukua")
+else:
+    print("decimali luku on", desimaali);
+   
