042512d3c360f9d7fbc7a17910dea3df2e8ddfdd
its me <76001700+whoiszacky@users.noreply.github.com>
2023-09-10 17:17:57 +0300

    Update harjoitusTehtava6.py

--- harjoitusTehtava6.py
+++ harjoitusTehtava6.py
@@ -9,3 +9,19 @@ def poistaDuplikaatit(lista):
 lista = [1, 1, 2, 3, 1, 5, 2, 4, 3]
 uniikit = poistaDuplikaatit(lista)
 print(uniikit)
+
+
+#hap fudud blase malaha wax function ah 
+"""
+Lista = [1, 1, 2, 3, 1, 5, 2, 4, 3]
+
+uniqueList = []
+for item in Lista:
+    if Lista.count(item) > 1:
+        while item in Lista:
+            Lista.remove(item)
+    uniqueList.append(item)
+
+print(uniqueList)"""
+
+
